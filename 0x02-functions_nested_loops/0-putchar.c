97cc609ab146e982970b3b73c0c5b1963e1210a9
kendo005 <kennzhood@yahoo.com>
2022-12-19 08:20:30 -0800

    2-largest_number.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,14 +9,14 @@ int main(void)
 
 {
 
-        _putcher(');
-        _putcher('p');
-        _putcher('u');
-        _putcher('t');
-        _putcher('c');
-        _putcher('h');
-        _putcher('a');
-        _putcher('r');
-        _putcher('\n');
-        return (0);
+_putcher(');
+_putcher('p');
+_putcher('u');
+_putcher('t');
+_putcher('c');
+_putcher('h');
+_putcher('a');
+_putcher('r');
+_putcher('\n');
+return (0);
 }
