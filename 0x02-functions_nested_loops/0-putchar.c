0feeb7953fdd5da8d0c551903539adb66c376330
kendo005 <kennzhood@yahoo.com>
2022-12-19 08:28:05 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,21 @@
-#include <main.h>
-
-/**
- * main - Entry point
- * Return: Always 0
- */
-
-int main(void)
-
-{
-
-_putcher(');
-_putcher('p');
-_putcher('u');
-_putcher('t');
-_putcher('c');
-_putcher('h');
-_putcher('a');
-_putcher('r');
-_putcher('\n');
-return (0);
-}
+#include "holberton.h" 
+ /** 
+  * main - this funtion show me in the opuput "Holberton" 
+  * 
+  * Return: Always 0. 
+  */ 
+  
+ int main(void) 
+ { 
+         _putchar('H'); 
+         _putchar('o'); 
+         _putchar('l'); 
+         _putchar('b'); 
+         _putchar('e'); 
+         _putchar('r'); 
+         _putchar('t'); 
+         _putchar('o'); 
+         _putchar('n'); 
+         _putchar('\n'); 
+         return (0); 
+ }
