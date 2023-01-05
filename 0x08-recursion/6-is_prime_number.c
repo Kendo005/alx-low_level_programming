dd312a63e65f3c4aebf9888f755622664346c3ec
kendo005 <kennzhood@yahoo.com>
2023-01-05 05:44:34 -0800

    task 100.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,50 +1,54 @@
 #include "main.h"
 
 /**
- * is_divisible - tests whether the number is divisible
- * @n: the number being tested
- * @div: the divisor
- *
- * Description: The function finds whether the numbeer is divisible
- * by 2 and return the value
- * Return: result
+ * evaluate_num - recursion loop
+ * @num: num
+ * @iterator: number to iterate
+ * Return: return 1 or 0
  */
-int is_divisible(int n, int div)
-{
-	if (n % div == 0)
-	{
-		return (0);
-	}
 
-	if (div == n / 2)
+int evaluate_num(int num, int iterator)
+{
+	if (iterator == num -1)
 	{
 		return (1);
 	}
 
-	return (is_divisible(n, div + 1));
+	else if (num % iterator == 0)
+	{
+		return (0);
+	}
+
+	if (num % iterator != 0)
+	{
+		return (evaluate_num(num, iterator + 1));
+	}
+	        return (0);
 }
 
 /**
- * is_prime_number - is the integer a prime number
- * @n: the number being tested
- *
- * Description: The function tests whether is a prime number
- * and returns the appropriate value.
- * Return: result
+ * is_prime_number - evaluate prime or not
+ * @num: number 
+ * Return: return 1 prime - return 0 otherwise
  */
-int is prime_number(int n)
-{
-	int div = 2;
 
-	if (n <= 1)
+int is prime_number(int num)
+{
+	int iterator;
+	iterator = 2;
+
+	/* only greater than 2*/
+	if (num < 2)
 	{
 		return (0);
 	}
 
-	if (n <= 3)
+	if (num == 2)
 	{
 		return (1);
 	}
 
-	return (is_divisible(n, div));
+	return (evaluate_num(num, iterator));
 }
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																															
+
