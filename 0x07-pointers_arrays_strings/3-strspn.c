84ea307e79082d6b10cf3cdeedf4073fdda07259
kendo005 <kennzhood@yahoo.com>
2023-01-07 05:49:07 -0800

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strpbrk - searches a string for any of a set of bytes.
+ * _strpn - searches a string for any of a set of bytes.
  * @s: string to be scanned
  * @accept: string containing characters to match
  * Description: This function finds the first character in the string s
@@ -9,7 +9,7 @@
  * This does not include terminating null-characters.
  * Return: pointer to the byte in s that matches one of the bytes in accept
  */
-char *_strpbrk(char *s, char *accept)
+char *_strpn(char *s, char *accept)
 {
 	int i, j;
 
